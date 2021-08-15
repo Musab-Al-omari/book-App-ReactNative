037622f96f69f1c00c789972091153a0f815ff1b
Musab-Al-omari <musabsomari@gmail.com>
2021-08-16 00:30:22 +0300

    new

--- scr/HomeScreen.jsx
+++ scr/HomeScreen.jsx
@@ -1,12 +1,27 @@
 import React, { useState } from 'react';
 import { Text } from 'react-native';
 import SearchBar from './component/SearchBar';
+// import Yelp from './api/Yelp';
+import axios from 'axios';
 export default function HomeScreen() {
   const [term, setTerm] = useState('');
   console.log(term);
 
   const onSubmit = function () {
-    console.log('hello');
+    axios
+      .get('https://api.yelp.com/v3/businesses/search', {
+        headers: {
+          Authorization:
+            'Bearer zf5OEAXCt7kL86NV3gnPH-BMLkvdXVitQDb08qgeYzmbQhblC9NMN9xVDsuY-rtbvGmQc-IIqtKMeMDp0EUem_8PZUOM4DnMAkPbDYajCLTBZ-gVaFy0OyKLrNRoYHYx',
+        },
+        params: { location: 'jordan' },
+      })
+      .then((data) => {
+        console.log(data);
+      });
+    // let res = await Yelp.get('/search', {
+    //   params: { location: 'jordan' },
+    // });
   };
   return (
     <>
