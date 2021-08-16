fecad4f1c0f588d6599879793238263c6da5b4cc
Musab-Al-omari <musabsomari@gmail.com>
2021-08-17 01:39:36 +0300

    reverse my project

--- scr/HomeScreen.jsx
+++ scr/HomeScreen.jsx
@@ -1,36 +1,48 @@
 import React, { useState } from 'react';
-import { Text } from 'react-native';
+import { Text, Alert } from 'react-native';
 import SearchBar from './component/SearchBar';
-// import Yelp from './api/Yelp';
-import axios from 'axios';
+import Anime from './api/Anime';
+// import axios from 'axios';
 export default function HomeScreen() {
   const [term, setTerm] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('anime');
+  const [resultArray, setResultArray] = useState([]);
   console.log(term);
-
+  const createTwoButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ]);
   const onSubmit = function () {
-    axios
-      .get('https://api.yelp.com/v3/businesses/search', {
-        headers: {
-          Authorization:
-            'Bearer zf5OEAXCt7kL86NV3gnPH-BMLkvdXVitQDb08qgeYzmbQhblC9NMN9xVDsuY-rtbvGmQc-IIqtKMeMDp0EUem_8PZUOM4DnMAkPbDYajCLTBZ-gVaFy0OyKLrNRoYHYx',
-        },
-        params: { location: 'jordan' },
-      })
+    if (term == '') {
+      createTwoButtonAlert();
+      console.log('hi');
+      return;
+    }
+    Anime.get(`${selectedCategory}`, {
+      params: {
+        q: term,
+      },
+    })
       .then((data) => {
-        console.log(data);
-      });
-    // let res = await Yelp.get('/search', {
-    //   params: { location: 'jordan' },
-    // });
+        console.log(data.data.results);
+      })
+      .catch((err) => console.log(err));
   };
   return (
     <>
       <SearchBar
-        term={term}
+        selectedCategory={selectedCategory}
+        onSelected={(itemValue) => setSelectedCategory(itemValue)}
         onSubmit={onSubmit}
         onChange={(newTerm) => setTerm(newTerm)}
       />
-      <Text>hello </Text>
+      {resultArray && resultArray.length}
+      <Text>asds </Text>
     </>
   );
 }
--- scr/component/SearchBar.jsx
+++ scr/component/SearchBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import { Feather } from '@expo/vector-icons';
 const styles = StyleSheet.create({
   background: {
@@ -12,26 +13,46 @@ const styles = StyleSheet.create({
   Feather: {
     flex: 1,
     alignSelf: 'center',
-    marginHorizontal: 10,
+    marginHorizontal: 5,
   },
   TextInput: {
-    flex: 9,
+    flex: 5,
     fontSize: 20,
   },
+  Picker: {
+    flex: 3,
+    backgroundColor: 'rgb(100, 59, 59)',
+  },
 });
-export default function SearchBar({ term, onChange, onSubmit }) {
+
+export default function SearchBar({
+  selectedCategory,
+  onSelected,
+  onChange,
+  onSubmit,
+}) {
   return (
-    <View style={styles.background}>
-      <Feather style={styles.Feather} name="search" size={35} color="black" />
-      <TextInput
-        autoCapitalize="none"
-        autoCorrect={false}
-        autoFocus={true}
-        style={styles.TextInput}
-        onChangeText={onChange}
-        onSubmitEditing={onSubmit}
-        placeholder="Search"
-      />
+    <View>
+      <View style={styles.background}>
+        <Feather style={styles.Feather} name="search" size={35} color="black" />
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          autoFocus={true}
+          style={styles.TextInput}
+          onChangeText={onChange}
+          onSubmitEditing={onSubmit}
+          placeholder="Search"
+        />
+        <Picker
+          style={styles.Picker}
+          selectedValue={selectedCategory}
+          onValueChange={onSelected}
+        >
+          <Picker.Item label="Anime" value="anime" />
+          <Picker.Item label="Manga" value="manga" />
+        </Picker>
+      </View>
     </View>
   );
 }
