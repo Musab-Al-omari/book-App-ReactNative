191f961b2feb37adfe4852ac016ff68da60fb752
Musab-Al-omari <musabsomari@gmail.com>
2021-08-24 04:45:48 +0300

    helped by ali

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator, } from '@react-navigation/stack'
+import { createStackNavigator } from '@react-navigation/stack'
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './scr/HomeScreen'
 import ItemScreen from './scr/ItemScreen'
